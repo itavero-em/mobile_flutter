6728096a22f12a2e68b8a9546383f14961910309
Benjamin Hirschkorn <Benjamin Hirschkorn>
2022-08-30 21:20:31 +0200

    Anbindung von Scandit

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,31 @@
+/*
+ * This file is part of the Scandit Data Capture SDK
+ *
+ * Copyright (C) 2020- Scandit AG. All rights reserved.
+ */
+
 import 'package:flutter/material.dart';
 import 'package:itavero_mobile/provider/connection_provider.dart';
+import 'package:itavero_mobile/views/scanning/barcode_scanner_view.dart';
 import 'package:itavero_mobile/views/settings/settings_view.dart';
 import 'package:provider/provider.dart';
+import 'package:scandit_flutter_datacapture_barcode/scandit_flutter_datacapture_barcode.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-
-void main() => runApp(MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (context) => ConnectionProvider(),
-        )
-      ],
-      child: const ItaveroMobile(),
-    )
-);
+void main() async
+{
+  WidgetsFlutterBinding.ensureInitialized();
+  await ScanditFlutterDataCaptureBarcode.initialize();
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(
+        create: (context) => ConnectionProvider(),
+      )
+    ],
+    child: const ItaveroMobile(),
+  ));
+}
 
 class ItaveroMobile extends StatelessWidget {
   const ItaveroMobile({Key? key}) : super(key: key);
@@ -52,6 +64,16 @@ class _MobileAppState extends State<MobileApp> {
           SettingsView(),
         ],
       ),
+
+    ),
+    Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children:  [
+          BarcodeScannerView(),
+        ],
+      ),
+
     ),
   ];
 
@@ -79,6 +101,10 @@ class _MobileAppState extends State<MobileApp> {
           BottomNavigationBarItem(
             icon: Icon(Icons.settings),
             label: 'Einstellungen',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.document_scanner_outlined),
+            label: 'Scan',
           ),
         ],
         currentIndex: _selectedIndex,
