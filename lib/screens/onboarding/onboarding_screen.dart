fb6d4914d22171a229776118457f9a711b0aca89
Benjamin Hirschkorn <Benjamin Hirschkorn>
2022-08-31 19:43:02 +0200

    Onboarding Screen

--- lib/screens/onboarding/onboarding_screen.dart
+++ lib/screens/onboarding/onboarding_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 //https://www.youtube.com/watch?v=c4dsvckUA1Q
 //https://undraw.co/illustrations
 class OnBoardingScreen extends StatelessWidget {
@@ -11,7 +10,8 @@ class OnBoardingScreen extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: const [
-          Text("Willkommen"),
+          // Text("Willkommen"),
+          // SvgPicture.asset("assets/undraw_real_time_sync_re_nky7.svg")
           //Image.asset("assets/undraw_real_time_sync_re_nky7.svg", height: 300,)
         ],
       ),
