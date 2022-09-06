3e827aa0464b83bec3a0f3161a03d2a8caeee4f3
Guido Klante <guido@klante.org>
2022-09-06 15:27:55 +0200

    Test mit Todos

--- lib/main.dart
+++ lib/main.dart
@@ -50,17 +50,7 @@ class ItaveroMobile extends StatelessWidget {
     );
   }
 
-// TODO: Add integration test for TodoActionsMain.
-//
-// Code that interface with external data have been separated into their own modules.
-// These includes:
-//
-// - `DataStore`
-// - `CodeRepository`
-// - `TaskManagementSystem`
-//
-// They can be mocked by creating a mock version using `__mocks__` folder.
-// https://jestjs.io/docs/en/manual-mocks
+
 }
 
 class MobileApp extends StatefulWidget {
