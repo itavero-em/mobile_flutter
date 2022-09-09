7246109a56aa47388c5d75ae897766e27f62acd1
Guido Klante <guido@klante.org>
2022-09-09 15:56:13 +0200

    Bearbeiten der Verbindung

--- lib/screens/connections/connection_list_screen.dart
+++ lib/screens/connections/connection_list_screen.dart
@@ -34,14 +34,15 @@ class _ConnectionListScreenState extends State<ConnectionListScreen> {
                 return Slidable(
                   // Specify a key if the Slidable is dismissible.
                   key: ValueKey(index),
-
                   // The start action pane is the one at the left or the top side.
                   startActionPane: ActionPane(
                     // A motion is a widget used to control how the pane animates.
                     motion: const ScrollMotion(),
 
                     // A pane can dismiss the Slidable.
-                    dismissible: DismissiblePane(onDismissed: () {}),
+                    //dismissible: DismissiblePane(onDismissed: () {
+                    //  print('SDSDDS');
+                    //  }),
 
                     // All actions are defined in the children parameter.
                     children: [
